3b6dd017abcdd8c37bce5edd4e740eddb0969057
Max Carvajal <jcarvajal@crimson.ua.edu>
2019-09-11 22:41:37 -0500

    Initial Commit.  Works with 'xdy' statements

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+//use regex::Regex;
+use rand::Rng;
+use std::env;
+
+fn interpret_roll_statement(roll_statement: &String) -> u32 {
+    //let dieRollRegex = Regex::new(r"(\d+)?d\d+").unwrap();
+    //let matchedDieRolls = dieRollRegex.captures(rollStatement).unwrap();
+    //println!("{:?}", matchedDieRolls);
+    //rollStatement.split_off(rollStatement.find('d').expect("'d' not found."));
+    let dindex: usize = roll_statement.find('d')
+        .expect("malformed rollStatement: 'd' not found");
+    let num_dice: u32 = roll_statement[..dindex].parse::<u32>().unwrap();
+    let die_size: u32 = roll_statement[dindex+1..].parse::<u32>().unwrap();
+
+    let mut total: u32 = 0;
+    for _ in 1..num_dice {
+        total += rand::thread_rng().gen_range(1, die_size);
+    }
+    return total;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let roll_statement: &String = &args[1];
+    println!("{}", roll_statement);
+    println!("{}", interpret_roll_statement(roll_statement));
+}
